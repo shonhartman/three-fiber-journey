7ad8ed896393b89b2c0fe05a7f77201ec419b7d6
shonhartman <shaun@bighuman.com>
2022-11-06 17:19:43 -0500

    first scene

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience';
+import * as THREE from 'three'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas
+        dpr={ [ 1, 2 ] }
+        gl={ {
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputEncoding: THREE.sRGBEncoding
+        } }
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 3, 2, 6 ]
+        } }
+    >
+        <Experience/>
+      </Canvas>
+    </>
   );
 }
 
