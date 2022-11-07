3b6b37fb13712c9e8af82b617a72fb68d1152b9a
shonhartman <shaun@bighuman.com>
2022-11-07 18:37:31 -0500

    scene + new react config style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber';
 import Experience from './Experience';
-import * as THREE from 'three'
 
 function App() {
   return (
     <>
-      <Canvas
-        dpr={ [ 1, 2 ] }
-        gl={ {
-            antialias: true,
-            toneMapping: THREE.ACESFilmicToneMapping,
-            outputEncoding: THREE.sRGBEncoding
-        } }
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ 3, 2, 6 ]
-        } }
-    >
+      <Canvas>
         <Experience/>
       </Canvas>
     </>
